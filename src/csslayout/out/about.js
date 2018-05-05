b3a97cf759a84e2725bee15159054a45412b8e45
mahongquan <mahongquan@sina.com>
2018-05-05 12:19:50 +0800

    ren

---
+++ src/csslayout/out/about.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+export default class about extends Component<Props> {
+  render() {
+    return (
+    <div id="container">
+<h1 className="content">about this site</h1>
+<style jsx="true">{`
+  #credits {
+    text-align: center;
+  }
+  #credits img {
+    vertical-align: middle;
+    margin: 0 20px;
+    width: 25%;
+    max-width: 300px;
+  }
+  #credits a:hover {
+    background-color: transparent;
+  }
+  .content {
+    text-align: center;
+  }
+`}</style>
+<div id="credits">
+<a ><img alt="Greg Smith" src="#"/></a>
+<a ><img alt="Isaac Durazo" src="#"/></a>
+<a ><img alt="Bocoup" src=".//images/bob.png"/></a>
+</div>
+<div className="content">
+  Written and built by <a >Greg Smith</a>. Design by <a >Isaac Durazo</a>. We work at <a >Bocoup</a>.
+</div>
+<div className="content">
+<a >
+    Issues / Feedback
+  </a>
+</div>
+</div>
+    );
+  }
+}
