7619665f5e5c67a54d2384645830b8124c25d7d2
Your Name <you@example.com>
2018-11-24 11:10:50 +0800

    package.json

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -33,22 +33,12 @@ import {
   Switch
 } from 'react-router-dom';
 import createHashHistory from 'history/createHashHistory';
-let ipcRenderer;
-if (window.require) {
-  ipcRenderer = require('electron').ipcRenderer; //
-}
 const history = createHashHistory({
   hashType: 'slash', // the default
 });
 export default class Root extends Component<Props> {
   constructor() {
     super();
-    if (ipcRenderer) {
-      ipcRenderer.on('goback', () => {
-        console.log(history);
-        history.goBack();
-      });
-    }
   }
   render() {
     const theme={red:"#D64078",green:"#96C02E",orange:"#FDC72F"};
